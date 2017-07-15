c10611e78820900b45d8831b476f920b527a7fdf
Vadow <alex.stelzig@hotmail.fr>
2017-07-15 13:59:31 -0400

    finished settings and help

--- app/src/main/java/a7967917_7698299/videogameshopapplication/fragments/HelpFragment.java
+++ app/src/main/java/a7967917_7698299/videogameshopapplication/fragments/HelpFragment.java
@@ -5,24 +5,42 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import a7967917_7698299.videogameshopapplication.MainActivity;
 import a7967917_7698299.videogameshopapplication.R;
 import a7967917_7698299.videogameshopapplication.database.DatabaseManager;
+import a7967917_7698299.videogameshopapplication.helper.Helper;
 import a7967917_7698299.videogameshopapplication.variables.OrderVariables;
 
 /**
  * Created by alex on 2017-06-24.
  */
 
-public class HelpFragment extends Fragment{
+public class HelpFragment extends Fragment {
 
 
     private View view;
+    private ListView listView;
+    private ScrollView scrollView;
 
+    private TextView aboutTextView;
+    private TextView shoppingTextView;
+    private TextView ordersTextView;
+    private TextView accountTextView;
 
     private DatabaseManager databaseManager;
+
+    private View viewToScrollTo = null;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,10 +52,56 @@ public class HelpFragment extends Fragment{
 
         databaseManager = DatabaseManager.getInstance();
 
-        ((Button) view.findViewById(R.id.test)).setOnClickListener(new View.OnClickListener() {
+        // used for testing - create an order. Also uncomment the button in the help fragment
+//        ((Button) view.findViewById(R.id.test)).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                ((MainActivity)getActivity()).createOrderFromCartItems("test","test","test", OrderVariables.STATUS.DELIVERED, 1234, "test",  2,2,"test","test","test","test","test", true );
+//            }
+//        });
+
+
+        listView = (ListView) view.findViewById(R.id.fragment_help_listview);
+        scrollView = (ScrollView) view.findViewById(R.id.fragment_help_scrollview);
+
+        aboutTextView = (TextView) view.findViewById(R.id.fragment_help_about_textview);
+        shoppingTextView = (TextView) view.findViewById(R.id.fragment_help_shpping_textview);
+        ordersTextView = (TextView) view.findViewById(R.id.fragment_help_orders_textview);
+        accountTextView = (TextView) view.findViewById(R.id.fragment_help_account_textview);
+
+        final List<String> options = new ArrayList<>();
+        options.add(getResources().getString(R.string.about));
+        options.add(getResources().getString(R.string.shopping_and_order));
+        options.add(getResources().getString(R.string.viewing_orders));
+        options.add(getResources().getString(R.string.account_and_settings));
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, options);
+
+        listView.setAdapter(arrayAdapter);
+
+        Helper.setListViewHeightBasedOnChildren(listView);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                ((MainActivity)getActivity()).createOrderFromCartItems("test","test","test", OrderVariables.STATUS.DELIVERED, 1234, "test",  2,2,"test","test","test","test","test", true );
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                if (position == options.indexOf(getResources().getString(R.string.about))) {
+                    viewToScrollTo = aboutTextView;
+                } else if (position == options.indexOf(getResources().getString(R.string.shopping_and_order))) {
+                    viewToScrollTo = shoppingTextView;
+                } else if (position == options.indexOf(getResources().getString(R.string.viewing_orders))) {
+                    viewToScrollTo = ordersTextView;
+                } else {
+                    viewToScrollTo = accountTextView;
+                }
+
+                scrollView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        scrollView.smoothScrollTo(0, viewToScrollTo.getTop());
+                    }
+                });
+
             }
         });
 
--- app/src/main/java/a7967917_7698299/videogameshopapplication/fragments/SettingsFragment.java
+++ app/src/main/java/a7967917_7698299/videogameshopapplication/fragments/SettingsFragment.java
@@ -1,10 +1,17 @@
 package a7967917_7698299.videogameshopapplication.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import a7967917_7698299.videogameshopapplication.R;
 
@@ -12,10 +19,15 @@ import a7967917_7698299.videogameshopapplication.R;
  * Created by alex on 2017-06-24.
  */
 
-public class SettingsFragment extends Fragment{
+public class SettingsFragment extends Fragment {
 
 
     private View view;
+    private Spinner countrySpinner;
+    private Spinner languageSpinner;
+    private TextView nbResultsTextView;
+
+    private int nbResultsPerPage = 20;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,6 +36,69 @@ public class SettingsFragment extends Fragment{
         view = inflater.inflate(R.layout.fragment_settings, container, false);
         setHasOptionsMenu(true);
 
+        countrySpinner = (Spinner) view.findViewById(R.id.fragment_settings_country_spinner);
+        languageSpinner = (Spinner) view.findViewById(R.id.fragment_settings_country_language_spinner);
+        nbResultsTextView = (TextView) view.findViewById(R.id.fragment_settings_country_nbResults_textview);
+
+
+        initSpinners();
+
         return view;
+    }
+
+    private void initSpinners() {
+
+        String[] countries = new String[]{"Canada", "USA"};
+        ArrayAdapter countryArrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, countries);
+        countrySpinner.setAdapter(countryArrayAdapter);
+
+        String[] languages = new String[]{"Français", "English"};
+        ArrayAdapter languageArrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, languages);
+        languageSpinner.setAdapter(languageArrayAdapter);
+        languageSpinner.setSelection(1);
+
+        nbResultsTextView.setText(nbResultsPerPage + "");
+        nbResultsTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showQuickToggleDialog();
+            }
+        });
+
+
+    }
+
+
+    public void showQuickToggleDialog() {
+
+        View viewPicker = View.inflate(getContext(), R.layout.custom_number_picker_dialog, null);
+        AlertDialog.Builder db = new AlertDialog.Builder(getContext());
+        db.setView(viewPicker);
+        db.setTitle("Quick Toggle Review");
+
+        final NumberPicker np = (NumberPicker) viewPicker.findViewById(R.id.numberPicker1);
+
+        np.setMaxValue(100); // max value 100
+        np.setMinValue(5);   // min value 0
+        np.setWrapSelectorWheel(false);
+
+        np.setValue(nbResultsPerPage);
+
+        db.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // do nothing
+            }
+        });
+
+        db.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                nbResultsPerPage = (np.getValue());
+                nbResultsTextView.setText(nbResultsPerPage + "");
+            }
+        });
+        db.show();
+
+
     }
 }
